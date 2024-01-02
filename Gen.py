d64a3420e71cc5f052eac5f7f39c2b8820f3d5cf
Fleur41 <Muriithikings456@gmail.com>
2024-01-02 12:20:45 +0300

    Final program

--- Gen.py
+++ Gen.py
@@ -23,3 +23,8 @@ def create_pw(pw_length = 12):
         if (any(char in special_chars for char in pwd) and
             sum(char in digits for char in pwd) >= 2):
             pw_strong = True  
+
+    return pwd
+
+if __name__ == '__main__':
+    print(create_pw())
