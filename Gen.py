bacbc52d7f34ad79bd313f5d2bf4171e80c7f0cb
Fleur41 <Muriithikings456@gmail.com>
2024-01-02 12:05:19 +0300

    create pw

--- Gen.py
+++ Gen.py
@@ -4,3 +4,11 @@ Password Generator
 '''
 
 import secrets
+import string
+
+def create_pw(pw_length = 12):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_chars = string.punctuation
+
+    alphabet = letters + digits + special_chars
